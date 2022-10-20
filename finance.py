78959f3e4ba407d3ec5ca8acb97bca7758b7a595
Elgius <114131653+Elgius@users.noreply.github.com>
2022-10-20 11:56:19 +0500

    field display bug noticed

--- finance.py
+++ finance.py
@@ -1,23 +1,46 @@
 import csv
 
 
-#file_name = "Finance_to_work.xlsx"
+file_name = "for data work.csv"
 
-#fields = []
-#rows = []
+fields = []
+rows = []
+
+with open(file_name,"r") as file:
+    data = csv.reader = (file)
+
+    fields = next(data)
+
+    for row in data:
+        rows.append(row)
 
 
-Run = True
+print("here is the csv file for inspection: ")
+print("Field names are as follows: " + "," .join(field for field in fields ))
 
-while Run:
-    print("Welcome to the finance tracker!")
-    print("All data entered are going to be run on the notebooks cells where we will be looking into the data")
-    print("And the Graphs will be all diplayed here")
+for row in rows[:5]:
+    for col in row:
+        print("%10s"%col,end=" "),
+    print('\n')
 
-    choice = input("Do you want to keep running the program?:  ")
-    match choice:
-        case "y":
-            Run = False
-
-        case _: Run = True
-
+#
+#Run = True
+#
+#while Run:
+#    print("Welcome to the finance tracker!")
+#    print("All data entered are going to be run on the notebooks cells where we will be looking into the data")
+#    print("And the Graphs will be all diplayed here")
+#
+#    data_bank = {
+#
+#
+#    }
+#
+#    choice = input("Do you want to keep running the program?:  ")
+#    match choice:
+#        case "y":
+#            Run = False
+#
+#        case _: Run = True
+#
+#
