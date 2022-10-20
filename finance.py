b586f162eddb9489a58b82a7fd6fdea31312eb93
Elgius <114131653+Elgius@users.noreply.github.com>
2022-10-20 11:28:46 +0500

    basic system run

--- finance.py
+++ finance.py
@@ -1,10 +1,23 @@
+import csv
+
+
+#file_name = "Finance_to_work.xlsx"
+
+#fields = []
+#rows = []
+
 
 Run = True
 
 while Run:
-    print("hello world")
+    print("Welcome to the finance tracker!")
+    print("All data entered are going to be run on the notebooks cells where we will be looking into the data")
+    print("And the Graphs will be all diplayed here")
 
     choice = input("Do you want to keep running the program?:  ")
-    if choice == "y":
-        Run = False
+    match choice:
+        case "y":
+            Run = False
+
+        case _: Run = True
 
