a07789d53ec2c4530d626eb796bcdc88818ca345
Elgius <114131653+Elgius@users.noreply.github.com>
2022-10-20 15:08:26 +0500

    CSV file reading is working, further bugs cleared

--- finance.py
+++ finance.py
@@ -7,20 +7,31 @@ while Run:
     print("All data entered are going to be run on the notebooks cells where we will be looking into the data")
     print("And the Graphs will be all diplayed here")
 
-    data_bank = {}
+    Spending = input("What did you spend on?:  ")
+    Quantity = input("How many times did you buy it?:   ")
+    Money = input("How much did this cost?:   ")
+
+    fields = ["Name", "Quantity", "Cost($)"]
+    #fields = ['Name', 'Branch', 'Year']
+    row = [[Spending, Quantity, Money]]
+
+    with open("for data work.csv" , "w") as file:
+        pen = csv.writer(file)
+        pen.writerow(fields)
+        pen.writerows(row)
+
 
     with open("for data work.csv","r") as file:
         data = csv.reader(file)
-
         for lines in file:
             print(lines)
 
 
 
 
-    choice = input("Do you want to keep running the program?: [y/n] ")
-    choice = choice.lowercase()
-    match choice:
+    choice = input("Do you want to end the program?: [y/n] ")
+    answer = choice.lower()
+    match answer:
         case "y":
             Run = False
 
